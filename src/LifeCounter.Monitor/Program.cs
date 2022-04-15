46ac224e2dd72d874c1da91ce859bb839b4de0b5
bymse <egorstrelov@gmail.com>
2022-04-16 00:43:47 +0300

    Dashboard front start + views recompilation in dev mode

--- src/LifeCounter.Monitor/Program.cs
+++ src/LifeCounter.Monitor/Program.cs
@@ -14,6 +14,13 @@ builder.Services
     .UseUtilities("monitor")
     ;
 
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services
+        .AddControllersWithViews()
+        .AddRazorRuntimeCompilation();
+}
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +29,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
 }
 
 app.UseHttpsRedirection();
