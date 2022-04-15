ba6c3c9cdd660b067f1d9e763ac4b746f5fcdcc9
bymse <egorstrelov@gmail.com>
2022-04-16 00:03:43 +0300

    Change front entry files location + add watch mode

--- src/LifeCounter.ClientApp/build.js
+++ src/LifeCounter.ClientApp/build.js
@@ -1,13 +1,25 @@
 const path = require('path');
 const fs = require('fs');
 
+const getFiles = (dir) => fs
+  .readdirSync(dir)
+  .map(file => path.resolve(dir, file))
+
 const entryPoints = fs
   .readdirSync(path.resolve('entry'))
-  .map(file => path.resolve('entry', file));
+  .flatMap(dir => getFiles(path.join(__dirname, 'entry', dir)));
+
+fs.rmSync(path.join(__dirname, 'build'), {recursive: true, force: true});
+
+const watch = process.argv[2] === 'watch';
 
 require('esbuild').build({
   entryPoints,
   bundle: true,
   outdir: 'build',
-  minify: true
-}).catch(() => process.exit(1));
+  minify: true,
+  entryNames: watch ? '[dir]/[name].watch' : '[dir]/[name].[hash]',
+  watch
+})
+  .then(() => watch && console.log('Watching...'))
+  .catch(() => process.exit(1));
