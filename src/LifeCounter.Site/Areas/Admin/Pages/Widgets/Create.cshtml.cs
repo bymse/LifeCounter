268254b4952072354a9decaccce10518bfb5223a
bymse <egorstrelov@gmail.com>
2022-06-12 23:20:28 +0300

    Move layout partials to Pages/Shared + sidebar impl

---
+++ src/LifeCounter.Site/Areas/Admin/Pages/Widgets/Create.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LifeCounter.Site.Areas.Admin.Pages.Widgets;
+
+public class Create : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
