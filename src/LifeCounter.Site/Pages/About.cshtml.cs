c88a53b0ce36e2d219369f490d33799e9609611b
bymse <egorstrelov@gmail.com>
2022-06-18 00:09:49 +0300

    Main page layout + css

---
+++ src/LifeCounter.Site/Pages/About.cshtml.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LifeCounter.Site.Pages;
+
+public class About : PageModel
+{
+    
+}
---
+++ src/LifeCounter.Site/Pages/GetStarted.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LifeCounter.Site.Pages;
+
+public class GetStarted : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
